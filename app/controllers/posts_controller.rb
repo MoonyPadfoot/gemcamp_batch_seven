f4f0474a9cf610875962d797147994c351ea4e0a
Renier Antonio <eurekavixen@gmail.com>
2024-10-17 17:02:13 +0800

    added: kaminari pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.includes(:categories, :user).all
+    @posts = Post.includes(:categories, :user).page(params[:page]).per(2)
   end
 
   def new
